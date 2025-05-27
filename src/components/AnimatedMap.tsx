b48c9808c775599e555b0374b7a0cb75566e7d62
WlXPzEqki4 <PKI5KQ01K3@ecorpsystems.com>
2025-05-27 19:48:09 +0400

    Fix AnimatedMap component syntax

--- src/components/AnimatedMap.tsx
+++ src/components/AnimatedMap.tsx
@@ -1,1 +1,13 @@
-[Previous content of AnimatedMap.tsx shown above]
+import React from 'react';
+
+const AnimatedMap: React.FC = () => {
+  return (
+    <div className="w-full h-[400px] bg-gray-100 rounded-lg shadow-md p-4">
+      <div className="text-center text-gray-600">
+        Map visualization will be implemented here
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedMap;
